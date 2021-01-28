e9863a772062ec6246d739183a1e21cb7771b2c5
MarkoN <markonisevic.it@gmail.com>
2021-01-28 09:09:59 +0100

    Initial Commit

---
+++ custom/blowfish/BhgObhaqRznvy.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2021-01-27 22:51:28
+$key = array (
+  0 => 'f10befac-1ff0-5d9a-8b53-6011eebea061',
+);
---
+++ custom/modules/Connectors/metadata/display_config.php
@@ -0,0 +1,23 @@
+<?php
+// created: 2021-01-27 22:47:33
+$modules_sources = array (
+  'Accounts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Contacts' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Leads' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+  'Prospects' => 
+  array (
+  ),
+  'Opportunities' => 
+  array (
+    'ext_rest_insideview' => 'ext_rest_insideview',
+  ),
+);
