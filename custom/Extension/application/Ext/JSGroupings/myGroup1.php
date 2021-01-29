bfc246643be85c708cd2b4aaf95f07c7428761d5
MarkoN <markonisevic.it@gmail.com>
2021-01-29 10:32:16 +0100

    Task D. JsGroupings - This task was not succesfully completed. After creating new grouping file in /custom/Extension/application/Ext/JSGroupings/ directory, required .js files and running Admin > Repair > Quick Repair, system would not create ./custom/application/Ext/JSGroupings/jsgroups.ext.php extension, so running the Admin > Repair > Rebuild JS Grouping Files, whould not compile files to single payload at /cache/include/javascript/ directory, checked the file paths, and documentation for additional information multiple times but found no solution.

---
+++ custom/Extension/application/Ext/JSGroupings/myGroup1.php
@@ -0,0 +1,6 @@
+<?php
+
+    $js_groupings[] = $newGrouping = array(
+        'custom/assets/js/file1.js' => 'include/javascript/myGroup1.js',
+        'custom/assets/js/file2.js' => 'include/javascript/myGroup1.js',
+);
